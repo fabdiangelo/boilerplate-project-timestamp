b25570dd4f7b5a9bc09b58e25e4af79f17e62348
Fabricio Fuentes <fuentesfabricio17@gmail.com>
2024-12-31 19:40:27 -0300

    Solution

--- index.js
+++ index.js
@@ -1,27 +1,30 @@
-// index.js
-// where your node app starts
-
-// init project
 var express = require('express');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({optionsSuccessStatus: 200}));
 
-// http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}))
 
-// http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+app.get("/api", function (req, res) {
+  let date = new Date();
+  res.json({"unix": date.getTime(), "utc": date.toUTCString()});
+});
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/:date", function (req, res) {
+  let date = new Date (Number(req.params.date));
+  if(isNaN(Number(req.params.date))) {
+    date = new Date(req.params.date);
+  }
+  if (date.getTime() === null || isNaN(date.getTime())) res.json({ error : "Invalid Date" });
+  res.json({"unix": date.getTime(), "utc": date.toUTCString()});
 });
 
 
